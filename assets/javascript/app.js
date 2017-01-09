ce6918ceb9d98f7a4c6974f9ddf68fbc7a179ab2
Alex Murray <namurray1@gmail.com>
2017-01-09 05:46:02 -0500

    Added some files to html, css, and js. Will add the code that runs game shortly.

---
+++ assets/javascript/app.js
@@ -0,0 +1,4 @@
+// Remember to add a timer for the game as well as some images! The player will have 10 seconds to answer each question. 
+// There will be a total of 10 questions.
+// Need to add classes for text, questions, and answers.
+//
