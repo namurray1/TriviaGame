8fd0567c20aefabb8209fcb58f66437d292d8488
Alex Murray <namurray1@gmail.com>
2017-01-11 06:49:13 -0500

    added addition JS files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,12 @@
-// Remember to add a timer for the game as well as some images! The player will have 10 seconds to answer each question. 
-// There will be a total of 10 questions.
-// Need to add classes for text, questions, and answer
-// Do not remove this line. This line contains the code that will be used to populate the questions, choices, and text fields.
-//use jQuery to add some added effects to clicks like sounds and animation.
 function Question(text, choices, answer) {
 	this.text = text;
-	this.choices = choices;
+	this.choice = choice;
 	this.answer = answer;
 }
+
 Question.prototype.correctAnswer = function(choices) {
 	return choices === this.answer;
-};
+}
 
 function Quiz(questions) {
 	this.score = 0;
@@ -28,47 +24,49 @@ Quiz.prototype.isEnded = function() {
 
 Quiz.prototype.guess = function(answser) {
 	this.questionIndex++;
+
 	if(this.getQuestionIndex().correctAnswer(answser)) {
 		this.score++;
 	}
 };
 
-/*function populate() {
+function populate() {
 	if(quiz.isEnded()) {
-		//showScores()
-	} else {
+		//showScores();
+	} 
+
+	else {
 		//show question
 		var element = document.getElementById("question");
 		element.innerHTML = quiz.getQuestionIndex().text;
 
 		//show choices
-		var choices = quiz.getQuestionIndex().choices;
+		/*var choices = quiz.getQuestionIndex().choices;
 		for(var i = 0; i < choices.length; i++) {
 			var element = document.getElementById("choices" + i);
-			element.innerHTML = choices[i];
+			element.innerHTML = choices[i];*/
 		}
 	}
-};
-function showScores() {
+
+/*function showScores() {
 	var gameOverHtml = "<h1>"Result</h1>"
-	gameOverHtml += "he"
+	gameOverHtml += "he"*/
 }
-// Add 10 questions here plus a timer
+
 
 var questions = [
-	new Question(""),["","","",""],"", //Enter the question plus four answers using and array and include the actual answer last.
-	new Question(""),["","","",""],"", //Enter the question plus four answers using and array and include the actual answer last.  
-	new Question(""),["","","",""],"", //Enter the question plus four answers using and array and include the actual answer last.
-	new Question(""),["","","",""],"", //Enter the question plus four answers using and array and include the actual answer last.
-	new Question(""),["","","",""],"", //Enter the question plus four answers using and array and include the actual answer last.
-	new Question(""),["","","",""],"", //Enter the question plus four answers using and array and include the actual answer last.
-	new Question(""),["","","",""],"", //Enter the question plus four answers using and array and include the actual answer last.
-	new Question(""),["","","",""],"", //Enter the question plus four answers using and array and include the actual answer last.
-	new Question(""),["","","",""],"", //Enter the question plus four answers using and array and include the actual answer last.
-	new Question(""),["","","",""],"", //Enter the question plus four answers using and array and include the actual answer last.
+	new Question("What is an example of a non-semantic element?",["<form>","<table>","<article>","<div>"],"<div>"), 
+	new Question("All but one of the following is a block level element",["<div>","<h1>","<a>","<form>"],"<a>"),   
+	new Question("Which of these is not considered a head element?",["<meta>","<style>","<h3>","<script>"],"<h3>"),
+	new Question("One of these is not considered a list tag.",["<ul>","<p>","<dd>","<dl>"],"<p>"), 
+	new Question("Bootstraps grid system allows up to how many columns?",["12","16","10","8"],"12"), 
+	new Question("One of the following is not a grid class for BootStrap.",["xxl","xs","lg","md"],"xxl"), 
+	new Question("You should have a basic knowledge of HTML, CSS, and JavaScript before you learn what?",["Ruby on Rails","jQuery","SQL","Python"],"jQuery"),
+	new Question("The PHP script starts with?",["<?php","<!doctype>","{}","$"],"<?php"), 
+	new Question("Searching for errors in programming is called?",["decluttering","debugging","deplaning","declaring"],"debugging"), 
 	];
 
 	var quiz = new Quiz(questions);
 
 	populate();
-	*/
+	
